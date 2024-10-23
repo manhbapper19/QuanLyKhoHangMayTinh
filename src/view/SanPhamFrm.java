d541a84cb107e0050d1c42ceb12a900dbca2f618
fak_you <huynguyen.nh99@gmail.com>
2024-10-23 10:32:31 +0700

    add whole na lot of form

--- src/view/SanPhamFrm.java
+++ src/view/SanPhamFrm.java
@@ -104,7 +104,7 @@ public class SanPhamFrm extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(32, 32, 32)
                 .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 118, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 121, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addGap(49, 49, 49))
         );
@@ -118,7 +118,7 @@ public class SanPhamFrm extends javax.swing.JFrame {
                         .addComponent(jTextField1))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jButton1)
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(0, 23, Short.MAX_VALUE)))
                 .addContainerGap())
         );
 
