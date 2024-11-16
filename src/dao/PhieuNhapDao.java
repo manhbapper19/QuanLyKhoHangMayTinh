cd428ab9e3d6d30d9dfaba2852a8323d269a0338
fak_you <huynguyen.nh99@gmail.com>
2024-11-16 15:54:32 +0700

    add a note

--- src/dao/PhieuNhapDao.java
+++ src/dao/PhieuNhapDao.java
@@ -8,7 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+//add note
 public class PhieuNhapDAO {
     Connection conn = JDBC.getJDBCConnection();
     PreparedStatement ps;
